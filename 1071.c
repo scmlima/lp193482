5f15e9a7544b741f260ee8b77437cb2b46cd9bfe
scmlima <scmlima@unifesp.br>
2026-04-23 14:30:44 -0300

    Create 1071.c

---
+++ 1071.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+int main() {
+
+int X, Y, maior, menor, i, soma;
+soma = 0;
+scanf("%i %i", &X, &Y);
+
+if(X < Y){
+    maior = Y;
+    menor = X;
+} else {
+    maior = X;
+    menor = Y;
+}
+
+for(i = (menor + 1); i < maior; ++i){ //menor + 1 enquanto i < maior (filtra só os valores entre eles)
+    if (i % 2 != 0){ // o % aqui retorna o resto de uma divisão inteira
+        soma += i; // se o resto da divisão for diferente de 0, então é somado o valor de i (ou seja: os números ímpares)
+    }
+}
+
+printf("%i\n", soma);
+
+return 0;
+
+}
