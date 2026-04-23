e40d654e718e6e2aeec43589ed6d894e03dbe5a0
scmlima <scmlima@unifesp.br>
2026-04-23 14:50:06 -0300

    Update 1013.c

--- 1013.c
+++ 1013.c
@@ -3,7 +3,7 @@ Disciplina  : Lógica de Programação, turma IB, 2026S1
 Nome        : Sarah Cristine Moraes Lima
 Linguagem   : C
 Problema    : https://judge.beecrowd.com/pt/problems/view/1013
-Data        : DD/MM/2026
+Data        : 11/04/2026
 Objetivo    : Ler 3 valores e imprimir qual o valor maior.
 Aprendizado : uso de scanf e condicional if.
 -------------------------------------------------------------------------- */
