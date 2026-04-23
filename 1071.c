02deb17c3f5518884c67f0a2c0534fee4d68fb02
scmlima <scmlima@unifesp.br>
2026-04-23 14:42:51 -0300

    Update 1071.c

--- 1071.c
+++ 1071.c
@@ -1,3 +1,13 @@
+/* --------------------------------------------------------------------------
+Disciplina  : Lógica de Programação, turma IB, 2026S1
+Nome        : Sarah Cristine Moraes Lima
+Linguagem   : C
+Problema    : https://judge.beecrowd.com/pt/problems/view/1071
+Data        : 17/04/2026
+Objetivo    : Ler 2 numeros inteiros, calcular e mostrar a soma entre os ímpares entre eles.
+Aprendizado : uso de for.
+-------------------------------------------------------------------------- */
+
 #include <stdio.h>
 
 int main() {
