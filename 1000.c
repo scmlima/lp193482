27dde39cb978852cb7c52c7be7efeab8573ccad9
scmlima <scmlima@unifesp.br>
2026-04-23 14:37:28 -0300

    Update 1000.c

--- 1000.c
+++ 1000.c
@@ -1,3 +1,13 @@
+/* --------------------------------------------------------------------------
+Disciplina  : Lógica de Programação, turma IB, 2026S1
+Nome        : Sarah Cristine Moraes Lima
+Linguagem   : C
+Problema    : https://judge.beecrowd.com/pt/problems/view/1000
+Data        : DD/MM/2026
+Objetivo    : Imprimir "Hello World!" na tela.
+Aprendizado : introdução à linguagem C e uso de printf
+-------------------------------------------------------------------------- */
+
 #include <stdio.h>
 int main() {
   prinft("Hello World!\n");
