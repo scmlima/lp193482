829ec4239de41afab791b5815567cd23b5b367ec
scmlima <scmlima@unifesp.br>
2026-04-23 14:27:37 -0300

    Create 1013.c

---
+++ 1013.c
@@ -0,0 +1,29 @@
+#inclua <stdio.h>
+
+int main() {
+  int A, B, C;
+  scanf("%i", &A);
+  scanf("%i", &B);
+  scanf("%i", &C);
+
+  int AB = (A - B);
+
+  if (AB < 0){
+    AB *= -1;
+}
+
+  int MaiorAB = (A + B + AB)/2;
+
+  int ABC = (MaiorAB - C);
+
+  if (ABC < 0){
+    ABC *= -1;
+}
+
+  int MaiorABC = (MaiorAB + C + ABC)/2;
+
+  printf("%i eh o maior\n", MaiorABC);
+
+  return 0;
+
+}
