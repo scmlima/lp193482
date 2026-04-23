25f6e5cc7c5d0d4993239fe43b6d9a77d05ca499
scmlima <scmlima@unifesp.br>
2026-04-23 14:51:01 -0300

    Update 1000.c

--- 1000.c
+++ 1000.c
@@ -3,7 +3,7 @@ Disciplina  : Lógica de Programação, turma IB, 2026S1
 Nome        : Sarah Cristine Moraes Lima
 Linguagem   : C
 Problema    : https://judge.beecrowd.com/pt/problems/view/1000
-Data        : DD/MM/2026
+Data        : 08/04/2026
 Objetivo    : Imprimir "Hello World!" na tela.
 Aprendizado : introdução à linguagem C e uso de printf
 -------------------------------------------------------------------------- */
