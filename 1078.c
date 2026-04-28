320ddb081aec59e3cfd188417ab08f1f52d84949
scmlima <scmlima@unifesp.br>
2026-04-28 17:57:27 -0300

    Update 1078.c

--- 1078.c
+++ 1078.c
@@ -1,3 +1,13 @@
+/* --------------------------------------------------------------------------
+Disciplina  : Lógica de Programação, turma IB, 2026S1
+Nome        : Sarah Cristine Moraes Lima
+Linguagem   : C
+Problema    : https://judge.beecrowd.com/pt/problems/view/1078
+Data        : 28/04/2026
+Objetivo    : Imprimir uma tabuada com valores lidos.
+Aprendizado : processos matemáticos dentro de printf.
+-------------------------------------------------------------------------- */
+
 #include <stdio.h>
  
 int main() {
