6c027a08a3e0b4178a96610a4eeb81e47eed8d50
scmlima <scmlima@unifesp.br>
2026-04-29 21:38:29 -0300

    Update 1253.c

--- 1253.c
+++ 1253.c
@@ -1,3 +1,13 @@
+/* --------------------------------------------------------------------------
+Disciplina  : Lógica de Programação, turma IB, 2026S1
+Nome        : Sarah Cristine Moraes Lima
+Linguagem   : C
+Problema    : https://judge.beecrowd.com/pt/problems/view/1253
+Data        : 28/04/2026
+Objetivo    : transformar a senença de entrada em uma sentança dançante.
+Aprendizado : uso da biblioteca <string> 
+-------------------------------------------------------------------------- */
+
 #include <string.h>
 #include <stdio.h>
 
