3c34405ab23a21fb36cca2de076bfff443bcbd85
scmlima <scmlima@unifesp.br>
2026-05-05 08:42:03 -0300

    Create 1241.c

---
+++ 1241.c
@@ -0,0 +1,15 @@
+/* --------------------------------------------------------------------------
+Disciplina  : Lógica de Programação, turma IB, 2026S1
+Nome        : Sarah Cristine Moraes Lima
+Linguagem   : C
+Problema    : https://judge.beecrowd.com/pt/problems/view/1241
+Data        : 05/05/2026
+Objetivo    : 
+Aprendizado : 
+-------------------------------------------------------------------------- */
+
+#include <stdio.h>
+int main() {
+  
+  return 0;
+}
