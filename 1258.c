ac03edd1b4c17d819bbb2dde391724e410c73eb2
scmlima <scmlima@unifesp.br>
2026-05-21 14:26:50 -0300

    Create 1258.c

---
+++ 1258.c
@@ -0,0 +1,6 @@
+/*--------------------------------------------
+                anotações
+ - struct e typedef (a estudar e usar nesse código)
+ - vetor, estrutura e ordenação para resposta certa (o que é necessário para esse código)
+ - manipulação de arquivo e uso de vscode (aula que vem)
+ --------------------------------------------*/
