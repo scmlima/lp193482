f41c54822b9d8422ee130d21d3a3173f37bf5125
scmlima <scmlima@unifesp.br>
2026-05-21 14:29:54 -0300

    Update 1258.c

--- 1258.c
+++ 1258.c
@@ -1,3 +1,20 @@
+/* --------------------------------------------------------------------------
+Disciplina  : Lógica de Programação, turma IB, 2026S1
+Nome        : Sarah Cristine Moraes Lima
+Linguagem   : C
+Problema    : https://judge.beecrowd.com/pt/problems/view/1258
+Data        : 21/05/2026
+Objetivo    : 
+Aprendizado : 
+Dúvidas     : 
+-------------------------------------------------------------------------- */
+#include <stdio.h>
+int main{
+
+  return 0;
+}
+
+
 /*--------------------------------------------
                 anotações
  - struct e typedef (a estudar e usar nesse código)
