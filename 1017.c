e7aec561447394756157466fd1f2caabf35637ad
scmlima <scmlima@unifesp.br>
2026-05-22 20:48:11 -0300

    Update 1017.c

--- 1017.c
+++ 1017.c
@@ -1,3 +1,14 @@
+/* --------------------------------------------------------------------------
+Disciplina  : Lógica de Programação, turma IB, 2026S1
+Nome        : Sarah Cristine Moraes Lima
+Linguagem   : C
+Problema    : https://judge.beecrowd.com/pt/problems/view/1017
+Data        : 22/05/2026
+Objetivo    : Calcular a quantidade de combustível necessária numa viagem
+Aprendizado : 
+Dúvida      : por que o beecrowd implica tanto com o "\n"?
+-------------------------------------------------------------------------- */
+
 #include <stdio.h>
  
 int main() {
