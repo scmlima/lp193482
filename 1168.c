564334fbc5c985778e8187e309011387de530a60
scmlima <scmlima@unifesp.br>
2026-05-26 08:49:30 -0300

    Update 1168.c

--- 1168.c
+++ 1168.c
@@ -1,3 +1,14 @@
+/* --------------------------------------------------------------------------
+Disciplina  : Lógica de Programação, turma IB, 2026S1
+Nome        : Sarah Cristine Moraes Lima
+Linguagem   : C
+Problema    : https://judge.beecrowd.com/pt/problems/view/1168
+Data        : /05/2026
+Objetivo    : 
+Aprendizado : 
+Dúvidas     : 
+-------------------------------------------------------------------------- */
+
 #include <string.h>
 #include <stdio.h>
 
