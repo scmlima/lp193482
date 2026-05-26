b8bb24b258be668dc536f64f84d52f19ab6f6f10
scmlima <scmlima@unifesp.br>
2026-05-26 08:49:55 -0300

    Update 1179.c

--- 1179.c
+++ 1179.c
@@ -1,3 +1,14 @@
+/* --------------------------------------------------------------------------
+Disciplina  : Lógica de Programação, turma IB, 2026S1
+Nome        : Sarah Cristine Moraes Lima
+Linguagem   : C
+Problema    : https://judge.beecrowd.com/pt/problems/view/1179
+Data        : /05/2026
+Objetivo    : 
+Aprendizado : 
+Dúvidas     : 
+-------------------------------------------------------------------------- */
+
 #include <stdlib.h>
 #include <stdio.h>
 
