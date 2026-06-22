1263e0783d67c42611e7273c1ae808f6ff618c9a
scmlima <scmlima@unifesp.br>
2026-06-22 20:13:43 -0300

    Refactor C code to C++ with iostream

--- 1435.c
+++ 1435.c
@@ -1,18 +1,22 @@
 /* --------------------------------------------------------------------------
 Disciplina  : Lógica de Programação, turma IB, 2026S1
 Nome        : Sarah Cristine Moraes Lima
-Linguagem   : C
+Linguagem   : C++
 Problema    : https://judge.beecrowd.com/pt/problems/view/1435
 Data        : 22/06/2026
 Objetivo    : printar uma matriz conforme o tamanho inserido pelo usuário
 Aprendizado : mais dificil do que parece
 -------------------------------------------------------------------------- */
 #include <stdio.h>
+#include <iostream>
+#include <iomanip>
+
+using namespace std;
  
 int main() {
     int n;
  
-    while(scanf("%i", &n) != EOF && n != 0){
+        while(cin >> n && n != 0){
         
         if(n == 0){
             break;
@@ -44,19 +48,20 @@ int main() {
             for (int j = 0; j < n; j++){
 
                 if(j == 0){
-                    printf("%3i", matriz[i][j]);
+                    cout << setw(3) << matriz[i][j];
                 } else {
-                    printf(" %3i", matriz[i][j]);
+                    cout << " " << setw(3) << matriz[i][j];
                 }
             }
-            printf("\n");        
+            cout << "\n";     
         }
-        printf("\n");
+        cout << "\n";
  
     }
 
     return 0;
 }
+
 /*----------------------------------------------------------
                         anotações
   AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA
