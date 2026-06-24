322593290ed38f91af3fa8c4c185eb07c6536f4e
scmlima <scmlima@unifesp.br>
2026-06-24 10:56:31 -0300

    Update 1179.c

--- 1179.c
+++ 1179.c
@@ -4,57 +4,44 @@ Nome        : Sarah Cristine Moraes Lima
 Linguagem   : C
 Problema    : https://judge.beecrowd.com/pt/problems/view/1179
 Data        : /05/2026
-Objetivo    : 
-Aprendizado : 
+Objetivo    : separar valores pares e impares em diferentes vetores de 5 espaços
+Aprendizado : "reutilização" de espaço dos vetores
 Dúvidas     : 
 -------------------------------------------------------------------------- */
-//refazer até fixar
 
-#include <stdlib.h>
 #include <stdio.h>
+#include <stdlib.h>
 
-void imprime(char *nome, int *vetor, int n)
-{
-    for (int i = 0; i < n; ++i)
-    {
-        printf("%s[%d] = %d\n", nome, i, vetor[i]);
+void imprime(char *nome, int *vetor, int n){
+    for(int i = 0; i < n; i++){
+        printf("%s[%i] = %i\n", nome, i, vetor[i]);
     }
 }
 
-int main()
-{
-    int valor, par[5], impar[5], n_par, n_impar;
+int main(){
+    int valor, par[5], impar[5], npar, nimpar;
+    npar = 0; nimpar = 0;
 
-    n_par = 0;
-    n_impar = 0;
-    for (int i = 0; i < 15; ++i)
-    {
-        scanf("%d", &valor);
+    for(int i = 0; i < 15; i++){
+        scanf("%i", &valor);
 
-        if (abs(valor) % 2)
-        {
-            impar[n_impar++] = valor;
-
-            if (n_impar == 5)
-            {
-                imprime("impar", impar, n_impar);
-                n_impar = 0;
+        if(abs(valor)%2){
+            impar[nimpar++] = valor;
+            if(nimpar == 5){
+                imprime("impar", impar, nimpar);
+                nimpar = 0;
             }
-        }
-        else
-        {
-            par[n_par++] = valor;
-
-            if (n_par == 5)
-            {
-                imprime("par", par, n_par);
-                n_par = 0;
+        } else {
+            par[npar++] = valor;
+            if(npar == 5){
+                imprime("par", par, npar);
+                npar = 0;
             }
         }
     }
 
-    imprime("impar", impar, n_impar);
-    imprime("par", par, n_par);
-
+    imprime("impar", impar, nimpar);
+    imprime("par", par, npar);
+        
     return 0;
 }
